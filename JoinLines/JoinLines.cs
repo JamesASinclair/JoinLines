493d1cf189b1d66f2a5253aac393cc0a8e4c3414
James Sinclair <jamessinclair@hotmail.com>
2016-09-26 18:20:59 +0100

    initial commit of solution

---
+++ JoinLines/JoinLines.cs
@@ -0,0 +1,183 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="JoinLines.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using EnvDTE80;
+
+namespace JoinLines
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class JoinLines
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("8ed65bca-91d1-41a5-8072-a267c17c86ee");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoinLines"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private JoinLines(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static JoinLines Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new JoinLines(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var activeTextDocument = ActiveTextDocument;
+            if (activeTextDocument != null)
+            {
+                var textSelection = activeTextDocument.Selection;
+
+                // If the selection has no length, try to pick up the next line.
+                if (textSelection.IsEmpty)
+                {
+                    textSelection.LineDown(true);
+                    textSelection.EndOfLine(true);
+                }
+
+                const string pattern = @"[ \t]*\r?\n[ \t]*";
+                const string replacement = @" ";
+
+                // Substitute all new lines (and optional surrounding whitespace) with a single space.
+                SubstituteAllStringMatches(textSelection, pattern, replacement);
+
+                // Move the cursor forward, clearing the selection.
+                textSelection.CharRight();
+            }
+        }
+                
+        /// <summary>
+        /// Substitutes all occurrences in the specified text document of the specified pattern
+        /// string with the specified replacement string.
+        /// </summary>
+        /// <param name="textDocument">The text document.</param>
+        /// <param name="patternString">The pattern string.</param>
+        /// <param name="replacementString">The replacement string.</param>
+        internal static void SubstituteAllStringMatches(TextSelection textDocument, string patternString, string replacementString)
+        {
+            TextRanges dummy = null;
+            int lastCount = -1;
+            while (textDocument.ReplacePattern(patternString, replacementString, (int)(vsFindOptions.vsFindOptionsRegularExpression | vsFindOptions.vsFindOptionsMatchInHiddenText), ref dummy))
+            {
+                // it is possible that the replacements aren't actually being done. In such a case,
+                // we can detect the situation by seeing if the count always remains the same, and
+                // if so exiting early.
+                if (lastCount == dummy.Count)
+                {
+                    //WarningWriteLine("Forced a break out of TextDocumentHelper's SubstituteAllStringMatches for a document.");
+                    break;
+                }
+                lastCount = dummy.Count;
+            }
+        }
+
+        /// <summary>
+        /// Gets the currently active document, otherwise null.
+        /// </summary>
+        public Document ActiveDocument
+        {
+            get
+            {
+                try
+                {
+                    return ((DTE2)ServiceProvider.GetService(typeof(DTE))).ActiveDocument;
+                }
+                catch (Exception)
+                {
+                    // If a project property page is active, accessing the ActiveDocument causes an exception.
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the active text document, otherwise null.
+        /// </summary>
+        private TextDocument ActiveTextDocument => GetTextDocument(this.ActiveDocument);
+
+        /// <summary>
+        /// Attempts to get the TextDocument associated with the specified document.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <returns>The associated text document, otherwise null.</returns>
+        internal TextDocument GetTextDocument(Document document)
+        {
+            if (document == null)
+                return null;
+
+            return document.Object("TextDocument") as TextDocument;
+        }
+    }
+}
---
+++ JoinLines/JoinLinesPackage.cs
@@ -0,0 +1,75 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="JoinLinesPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace JoinLines
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(JoinLinesPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class JoinLinesPackage : Package
+    {
+        /// <summary>
+        /// JoinLinesPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "cd28a771-8c83-4f85-887e-bb0576f1da45";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoinLines"/> class.
+        /// </summary>
+        public JoinLinesPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            JoinLines.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
